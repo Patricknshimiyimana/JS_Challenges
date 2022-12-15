29a03bbce1a159df66fbfb4d57b172e50135fee3
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-12-15 15:19:06 +0200

    check if two strings are anagrams

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ It returns a new object with three methods;
  - remove(name) - remove an item ion the collection if exists, if it does not exist, nothing happens
 
  - getList() - returns an array of items so far
+
+
+### #Anagram Checker
+A Function to check whether two given strings are anagram of each other or not. 
+- An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, “abcd” and “dabc” are an anagram of each other.
+- The function returns `true` if two strings are anagrams, else `false`
---
+++ areAnagrams.js
@@ -0,0 +1,51 @@
+/* SOLUTION 1 */
+const areAnagrams = (str1, str2) => {
+  let sortedStr1 = str1.toLowerCase().split("").sort().join("");
+  let sortedStr2 = str2.toLowerCase().split("").sort().join("");
+
+  if (sortedStr1.length != sortedStr2.length) return false;
+  return sortedStr1 === sortedStr2;
+};
+
+console.log(areAnagrams("triangle", "integral"));
+console.log(areAnagrams("silent", "liste"));
+console.log(areAnagrams("silent", "listen"));
+console.log(areAnagrams("silent", "lister"));
+
+
+/* SOLUTION 2 */
+// function areAnagrams(stringA, stringB) {
+//     /*First, we remove any non-alphabet character using regex and convert
+//     convert the strings to lowercase. */
+//     stringA = stringA.replace(/[^\w]/g, "").toLowerCase()
+//     stringB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+//     //Get the character map of both strings
+//     const charMapA = getCharMap(stringA)
+//     const charMapB = getCharMap(stringB)
+
+//     /* Next, we loop through each character in the charMapA, 
+//     and check if it exists in charMapB and has the same value as
+//     in charMapA. If it does not, return false */
+//     for (let char in charMapA) {
+//         if (charMapA[char] !== charMapB[char]) {
+//             return false
+//         }
+//     }
+
+//     return true
+// }
+
+// function getCharMap(string) {
+//     // We define an empty object that will hold the key - value pairs.
+//     // let charMap = new Map // You can use this or an empty obj
+//     let charMap = {};
+
+//     /*We loop through each character in the string. if the character 
+//     already exists in the map, increase the value, otherwise add it 
+//     to the map with a value of 1 */
+//     for (let char of string) {
+//         charMap[char] = charMap[char] + 1 || 1
+//     }
+//     return charMap
+// }
